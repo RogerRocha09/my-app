67e16924ddaff2aafe0dc3bd610a352650c05d61
ROGER ROCHA <153238764+RogerRocha09@users.noreply.github.com>
2024-06-07 21:06:57 -0300

    certo

--- app/components/items.js
+++ app/components/items.js
@@ -1,7 +1,7 @@
 const Items = (props) => {
     return ( 
         <div>
-            <p><strong>{props.conteudo}</strong> <br/><p> Preço: {props.preco} <button>Comprar</button></p></p>
+            <p><strong>{props.conteudo}</strong> <br/> Preço: {props.preco} <button>Comprar</button></p>
         </div>
      );
 }
